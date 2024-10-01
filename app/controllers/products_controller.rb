1deed9f5c2ce56100f45af5ca34c2cac36fd61b8
Sajib Saha <sajib.saha@brainstation-23.com>
2024-10-01 18:22:04 +0600

    Last Commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,58 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: %i[ index show ]
+
+  # GET /products or /products.json
+  def index
+    @products = Product.all
+  end
+
+  # GET /products/1 or /products/1.json
+  def show
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products or /products.json
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: "Product was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /products/1 or /products/1.json
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: "Product was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /products/1 or /products/1.json
+  def destroy
+    @product.destroy!
+      redirect_to products_path, status: :see_other, notice: "Product was successfully destroyed."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+    end
+end
